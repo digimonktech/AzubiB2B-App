6648183c81828e24ae7a79baa2cb02bb6a6af278
Gaurav Jain <gauravjain@Gauravs-Mac-mini.local>
2025-12-17 10:45:29 +0530

    deeplink

--- ios/jobB2B/AppDelegate.swift
+++ ios/jobB2B/AppDelegate.swift
@@ -5,26 +5,85 @@ import ReactAppDependencyProvider
 
 @main
 class AppDelegate: RCTAppDelegate {
-  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+  ) -> Bool {
+
     self.moduleName = "jobB2B"
     self.dependencyProvider = RCTAppDependencyProvider()
-
-    // You can add your custom initial props in the dictionary below.
-    // They will be passed down to the ViewController used by React Native.
     self.initialProps = [:]
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
+  // 🔗 Deep link handler (custom scheme: jobb2b://)
+  override func application(
+    _ application: UIApplication,
+    open url: URL,
+    options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+  ) -> Bool {
+    return RCTLinkingManager.application(application, open: url, options: options)
+  }
+
+  // 🔗 Universal links / background / cold start
+  override func application(
+    _ application: UIApplication,
+    continue userActivity: NSUserActivity,
+    restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void
+  ) -> Bool {
+    return RCTLinkingManager.application(
+      application,
+      continue: userActivity,
+      restorationHandler: restorationHandler
+    )
+  }
+
   override func sourceURL(for bridge: RCTBridge) -> URL? {
-    self.bundleURL()
+    return self.bundleURL()
   }
 
   override func bundleURL() -> URL? {
 #if DEBUG
-    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+    return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
 #else
-    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+    return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
 #endif
   }
 }
+
+
+
+
+
+// import UIKit
+// import React
+// import React_RCTAppDelegate
+// import ReactAppDependencyProvider
+
+// @main
+// class AppDelegate: RCTAppDelegate {
+//   override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//     self.moduleName = "jobb2b"
+//     self.dependencyProvider = RCTAppDependencyProvider()
+
+//     // You can add your custom initial props in the dictionary below.
+//     // They will be passed down to the ViewController used by React Native.
+//     self.initialProps = [:]
+
+//     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+//   }
+
+//   override func sourceURL(for bridge: RCTBridge) -> URL? {
+//     self.bundleURL()
+//   }
+
+//   override func bundleURL() -> URL? {
+// #if DEBUG
+//     RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+// #else
+//     Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+// #endif
+//   }
+// }
